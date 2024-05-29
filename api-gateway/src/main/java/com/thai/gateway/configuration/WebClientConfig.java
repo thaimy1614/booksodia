d0097d3a93c4ee787901e07352bbd3f36ba8ca99
thaimy1614 <lxlthailxl@gmail.com>
2024-05-29 11:17:07 +0700

    add cors config for api-gateway service

--- api-gateway/src/main/java/com/thai/gateway/configuration/WebClientConfig.java
+++ api-gateway/src/main/java/com/thai/gateway/configuration/WebClientConfig.java
@@ -3,6 +3,9 @@ package com.thai.gateway.configuration;
 import com.thai.gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -15,6 +18,18 @@ public class WebClientConfig {
                 .baseUrl("http://localhost:8080/identity")
                 .build();
     }
+
+    @Bean
+    CorsWebFilter corsWebFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
     @Bean
     IdentityClient identityClient(WebClient webClient){
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
