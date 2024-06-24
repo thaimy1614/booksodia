2f2cbb2112cda66a5063626c6cea6517b9f0108d
thaimy1614 <lxlthailxl@gmail.com>
2024-06-24 10:52:44 +0700

    init post-service

---
+++ post-service/src/main/java/com/thai/post_service/PostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thai.post_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
---
+++ post-service/src/test/java/com/thai/post_service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai.post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
