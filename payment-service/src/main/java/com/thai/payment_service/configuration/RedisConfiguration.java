d2b3a4f0b2d2ae960b3904a2bd8571b8d8f96c62
thaimy1614 <lxlthailxl@gmail.com>
2024-06-28 23:42:22 +0700

    set redis service for cart

--- payment-service/src/main/java/com/thai/payment_service/configuration/RedisConfiguration.java
+++ payment-service/src/main/java/com/thai/payment_service/configuration/RedisConfiguration.java
@@ -1,5 +1,9 @@
 package com.thai.payment_service.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,10 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class RedisConfiguration {
@@ -33,6 +41,14 @@ public class RedisConfiguration {
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
 
+    @Bean
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+        simpleModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
+        return objectMapper.registerModule(simpleModule);
     }
 }
--- payment-service/src/main/java/com/thai/payment_service/model/Cart_Book.java
+++ payment-service/src/main/java/com/thai/payment_service/model/Cart_Book.java
@@ -1,10 +1,12 @@
 package com.thai.payment_service.model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EntityListeners;
 import lombok.Data;
 
 @Data
 @Embeddable
+@EntityListeners(CartListener.class)
 public class Cart_Book {
     private String bookId;
     private int quantity;
