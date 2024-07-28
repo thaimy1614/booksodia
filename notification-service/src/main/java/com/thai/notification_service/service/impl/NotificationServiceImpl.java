912a96b6714509d0a0dbec419421d6cbdb2b9fa3
thaimy1614 <lxlthailxl@gmail.com>
2024-07-29 00:04:05 +0700

    1 user - multi connection

--- notification-service/src/main/java/com/thai/notification_service/service/impl/NotificationServiceImpl.java
+++ notification-service/src/main/java/com/thai/notification_service/service/impl/NotificationServiceImpl.java
@@ -1,80 +1,100 @@
 package com.thai.notification_service.service.impl;
 
 import com.thai.notification_service.service.NotificationService;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.*;
 
 @Service
 @Slf4j
 public class NotificationServiceImpl implements NotificationService {
     private static final String HEARTBEAT_MESSAGE = "heartbeat";
-    private static final long HEARTBEAT_INTERVAL = 30000; // 30 seconds
-    private static final long TIMEOUT_DURATION = 60000; // 60 seconds
-    // TODO: Replace Hashmap with Redis for scale different instance
+    private static final long HEARTBEAT_INTERVAL = 30; // 30 seconds
+    private static final long TIMEOUT_DURATION = 60_000L; // 60 seconds
     private final ConcurrentHashMap<String, CopyOnWriteArrayList<SseEmitter>> emitters = new ConcurrentHashMap<>();
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
+    public NotificationServiceImpl() {
+        startHeartbeat();
+    }
+
     public void sendNotification(String userId, String notification) {
-        CopyOnWriteArrayList<SseEmitter> userEmitters = emitters.get(userId);
+        List<SseEmitter> userEmitters = emitters.get(userId);
         if (userEmitters != null) {
-            for (SseEmitter emitter : userEmitters) {
+            userEmitters.removeIf(emitter -> {
                 try {
                     emitter.send(notification);
+                    return false;
                 } catch (IOException e) {
-                    log.info(e.getMessage());
-                    userEmitters.remove(emitter);
+                    return true; // Remove emitter if an error occurs
                 }
-            }
+            });
         }
+        log.info("emitters: {}", emitters.toString());
     }
 
     public SseEmitter subscribe(String userId) {
-        CopyOnWriteArrayList<SseEmitter> userEmitters = emitters.get(userId);
-        // maximum 5 connects each user
-        if (userEmitters!=null && userEmitters.size() >= 5) {
+        List<SseEmitter> userEmitters = emitters.computeIfAbsent(userId, k -> new CopyOnWriteArrayList<>());
+        if (!userEmitters.isEmpty()) {
             userEmitters.clear();
         }
-        SseEmitter emitter = new SseEmitter(TIMEOUT_DURATION); // No timeout
-        emitters.computeIfAbsent(userId, k -> new CopyOnWriteArrayList<>()).add(emitter);
+
+        SseEmitter emitter = new SseEmitter(TIMEOUT_DURATION);
+        userEmitters.add(emitter);
 
         emitter.onCompletion(() -> removeEmitter(userId, emitter));
-        emitter.onTimeout(() -> {
-            removeEmitter(userId, emitter);
-        });
+        emitter.onTimeout(() -> removeEmitter(userId, emitter));
         emitter.onError((ex) -> removeEmitter(userId, emitter));
-//        startHeartbeat(userId, emitter);
+
         log.info(emitters.toString());
         return emitter;
     }
 
-
     private void removeEmitter(String userId, SseEmitter emitter) {
-        CopyOnWriteArrayList<SseEmitter> userEmitters = emitters.get(userId);
+        List<SseEmitter> userEmitters = emitters.get(userId);
         if (userEmitters != null) {
             userEmitters.remove(emitter);
-            log.info("Removed emitter for userId: {}", userId);
             if (userEmitters.isEmpty()) {
                 emitters.remove(userId);
-                log.info("All emitters removed for userId: {}", userId);
             }
         }
+        log.info("Removed emitter for userId: {}", userId);
     }
 
-    private void startHeartbeat(String userId, SseEmitter emitter) {
+    private void startHeartbeat() {
         scheduler.scheduleAtFixedRate(() -> {
-            try {
-                emitter.send(SseEmitter.event().data(HEARTBEAT_MESSAGE));
-            } catch (IOException e) {
-                // Handle error: possibly remove emitter, log information, etc.
-                removeEmitter(userId, emitter);
-            }
+            emitters.forEach((userId, userEmitters) -> {
+                userEmitters.removeIf(emitter -> {
+                    try {
+                        emitter.send(SseEmitter.event().data(HEARTBEAT_MESSAGE));
+                        return false;
+                    } catch (IOException e) {
+                        return true; // Remove emitter if an error occurs
+                    }
+                });
+            });
         }, 0, HEARTBEAT_INTERVAL, TimeUnit.SECONDS);
     }
 
+    @PreDestroy
+    public void onDestroy() {
+        log.info("Shutting down NotificationService...");
+        shutdown();
+    }
+
+    public void shutdown() {
+        scheduler.shutdown();
+        try {
+            if (!scheduler.awaitTermination(1, TimeUnit.SECONDS)) {
+                scheduler.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            scheduler.shutdownNow();
+        }
+    }
 }
-
-
