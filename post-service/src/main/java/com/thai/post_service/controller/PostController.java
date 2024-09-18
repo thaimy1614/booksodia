a56a43c2e64568ab7705d4a603365bf21b7fa898
thaimy1614 <lxlthailxl@gmail.com>
2024-09-18 14:34:40 +0700

    set restful api for post service

--- post-service/src/main/java/com/thai/post_service/controller/PostController.java
+++ post-service/src/main/java/com/thai/post_service/controller/PostController.java
@@ -1,4 +1,50 @@
 package com.thai.post_service.controller;
 
+import com.thai.post_service.dto.request.PostRequest;
+import com.thai.post_service.dto.response.PostResponse;
+import com.thai.post_service.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("${application.api.prefix}")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+
+    @GetMapping("")
+    public Page<PostResponse> getAllPosts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Page<PostResponse> getPostById(
+            @PathVariable String id
+            ) {
+        return null;
+    }
+
+    @PostMapping("")
+    public Page<PostResponse> createPost(
+            @RequestBody PostRequest postRequest) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Page<PostResponse> updatePost(
+            @PathVariable String id,
+            @RequestBody PostRequest postRequest) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Page<PostResponse> deletePostById(
+            @PathVariable String id) {
+        return null;
+    }
+
 }
---
+++ post-service/src/main/java/com/thai/post_service/service/impl/PostServiceImpl.java
@@ -0,0 +1,8 @@
+package com.thai.post_service.service.impl;
+
+import com.thai.post_service.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+}
