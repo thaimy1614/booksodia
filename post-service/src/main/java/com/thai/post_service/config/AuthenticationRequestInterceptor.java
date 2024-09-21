60590c5cd06d578feb89bce7c2531b31149306bf
thaimy1614 <lxlthailxl@gmail.com>
2024-09-21 20:16:31 +0700

    add auth interceptor for feign client

---
+++ post-service/src/main/java/com/thai/post_service/config/AuthenticationRequestInterceptor.java
@@ -0,0 +1,26 @@
+package com.thai.post_service.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+@Slf4j
+@Component
+public class AuthenticationRequestInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes();
+        assert servletRequestAttributes != null;
+        var authHeader = servletRequestAttributes.getRequest().getHeader("Authorization");
+        log.info("authHeader: {}", authHeader);
+        if (StringUtils.hasText(authHeader)) {
+            requestTemplate.header("Authorization", authHeader);
+        }
+    }
+}
