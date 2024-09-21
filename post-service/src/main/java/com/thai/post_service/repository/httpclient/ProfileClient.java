1434c50646fb59549cd7a28bef6bfcaa175fc740
thaimy1614 <lxlthailxl@gmail.com>
2024-09-21 20:20:29 +0700

    call api to check the relationship

--- post-service/src/main/java/com/thai/post_service/repository/httpclient/ProfileClient.java
+++ post-service/src/main/java/com/thai/post_service/repository/httpclient/ProfileClient.java
@@ -1,12 +1,15 @@
 package com.thai.post_service.repository.httpclient;
 
+import com.thai.post_service.config.AuthenticationRequestInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "profile-service", url = "${app.services.profile}", configuration = {AuthenticationRequestInterceptor.class})
 public interface ProfileClient {
-    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    Boolean createProfile(@RequestBody ProfileCreationRequest request);
+    @GetMapping(value = "/relationship/{user1}/{user2}")
+    Boolean createProfile(
+            @PathVariable String user1,
+            @PathVariable String user2
+    );
 }
